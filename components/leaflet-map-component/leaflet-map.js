60c6ba62e49674ce862e8a122be9129d38da033f
Miguel Pagola <mipagola@gmail.com>
2023-01-27 12:59:13 +0100

    feat: move adding map to component js

--- components/leaflet-map-component/leaflet-map.js
+++ components/leaflet-map-component/leaflet-map.js
@@ -1,7 +1,16 @@
 class LeafletMap extends HTMLElement{
 
     connectedCallback() {
-        this.textContent = 'Hello, World!';
+        const template = document.querySelector('template');
+        const node = document.importNode(template.content, true);
+        document.body.appendChild(node);
+
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
     }
 }
 
