66721236b7211f030f9dda7ed043788d6882ca63
Miguel Pagola <mipagola@gmail.com>
2023-03-21 17:39:48 +0100

    style: unify

--- components/leaflet-map-component/leaflet-map-load.js
+++ components/leaflet-map-component/leaflet-map-load.js
@@ -41,7 +41,7 @@ class LeafletMapLoad {
         return js;
     }
 
-    async #fetchLeafletCss() {
+    #fetchLeafletCss = async () => {
         const leafletCss = {
             url: 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css',
             integrity: 'sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI='
