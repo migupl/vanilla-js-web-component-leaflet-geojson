1e3c3802d3cbb54f3a1272434b76b74a7119c6b4
Miguel Pagola <mipagola@gmail.com>
2023-06-20 17:24:02 +0200

    feat: add draggable marker

--- js/example.js
+++ js/example.js
@@ -15,7 +15,7 @@ window.onload = (event) => {
             coordinates: [-0.09, 51.5]
         },
         properties: {
-            popupContent: "<b>Hello world!</b><br>I am a popupContent.",
+            popupContent: "<b>Hello world!</b><br>I am a popupContent on a draggable marker<br>Come on, try it!!!",
             icon: {
                 iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
                 shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -23,7 +23,8 @@ window.onload = (event) => {
                 iconAnchor: [12, 41],
                 popupAnchor: [1, -34],
                 shadowSize: [41, 41]
-            }
+            },
+            draggable: true
         }
     }
 
