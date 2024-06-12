a3772ba9c09d659602c5ba49004af7ed2a74333e
Miguel Pagola <mipagola@gmail.com>
2024-06-12 12:44:08 +0200

    refactor: move getting Leaflet style from CDN from private function to inner function

--- components/leaflet-map-component/leaflet-map-load.js
+++ components/leaflet-map-component/leaflet-map-load.js
@@ -82,9 +82,18 @@ class LeafletMapLoad {
     }
 
     getLeafletCss = () => {
+        const fetchLeafletCss = () => {
+            const leafletCss = {
+                url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+                integrity: 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY='
+            };
+
+            return this.#fetchCss(leafletCss.url)
+        }
+
         const el = document.createElement('style');
-        this.#fetchLeafletCss().then(
-            (css) => { el.innerText = css }
+        fetchLeafletCss().then(
+            (css) => { el.innerText = css}
         );
 
         return el;
@@ -131,15 +140,6 @@ class LeafletMapLoad {
 
         const css = await response.text();
         return css;
-    }
-
-    #fetchLeafletCss = () => {
-        const leafletCss = {
-            url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-            integrity: 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY='
-        };
-
-        return this.#fetchCss(leafletCss.url);
     }
 
     #getMarkerClusterStyle = cssFile => {
