5527f9e4ff79cb57bd522029a24a9f75394404a5
Miguel Pagola <mipagola@gmail.com>
2024-06-12 14:24:00 +0200

    refactor: include file content and remove it

--- components/leaflet-map-component/leaflet-map-features.js
+++ components/leaflet-map-component/leaflet-map-features.js
@@ -1,4 +1,24 @@
-import { LeafletMapFeature } from "./leaflet-map-feature.js";
+class LeafletMapFeature {
+
+    #map;
+
+    constructor(map) {
+        this.#map = map;
+    }
+
+    coordsToLatLng = coordinates => {
+        const latlng = L.GeoJSON.coordsToLatLng(coordinates);
+        this.#map.dispatchEvent(new CustomEvent('x-leaflet-map-geojson:include-latlng-to-fly', {
+            bubbles: true,
+            composed: true,
+            detail: {
+                latlng: latlng,
+            }
+        }));
+
+        return latlng;
+    }
+}
 
 class LeafletMapFeatures {
 
