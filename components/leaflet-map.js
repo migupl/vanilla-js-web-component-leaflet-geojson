420d0a2c33ce497d364882afb54830c5ca030718
Miguel Pagola <mipagola@gmail.com>
2024-06-13 10:29:04 +0200

    refactor: change scope of function

--- components/leaflet-map.js
+++ components/leaflet-map.js
@@ -11,21 +11,21 @@
         }
 
         connectedCallback() {
-            const css = mapElements.getWcStyleNode();
+            const css = this.#mapElements.getWcStyleNode();
             this.#appendChild(css);
 
             const [customStyleClass, customStyleFile] = this.#getCustomStyle();
 
-            const customCss = mapElements.getStyleNodeFrom(customStyleFile);
+            const customCss = this.#mapElements.getStyleNodeFrom(customStyleFile);
             this.#appendChild(customCss);
 
-            const markerClusterCss = mapElements.getMarkerClusterStyleNode();
+            const markerClusterCss = this.#mapElements.getMarkerClusterStyleNode();
             markerClusterCss.forEach(style => this.#appendChild(style))
 
-            const leafletCss = mapElements.getLeafletStyleNode();
+            const leafletCss = this.#mapElements.getLeafletStyleNode();
             this.#appendChild(leafletCss);
 
-            const map = mapElements.getWcNode();
+            const map = this.#mapElements.getWcNode();
             this.#addCustomStyleClass(customStyleClass, map);
             this.#appendChild(map);
             this.#initializeMap(map);
@@ -370,134 +370,134 @@
             popupAnchor: [1, -34],
             shadowSize: [41, 41]
         })
-    }
 
-    const mapElements = (() => {
-        const getRemoteCssFile = url => {
-            return fetch(url)
-                .then(response => response.text())
-                .catch(error => {
-                    const message = `An error has occured: ${response.status}`;
-                    throw new Error(message)
-                })
-        }
-
-        const getWcStyleNode = () => {
-            const el = document.createElement('style');
-            el.innerText =
-                ':host {' +
-                    'display: block;' +
-                '}' +
-                '#map {' +
-                    'position: absolute;' +
-                    'inset: 0px;' +
-                    'top: 0;' +
-                    'right: 0;' +
-                    'bottom: 0;' +
-                    'left: 0;' +
-                '}' +
-                '.leaflet-container {' +
-                    'max-width: 100%;' +
-                    'max-height: 100%;' +
-                '}' +
-                '.leaflet-control-zoom {' +
-                    'display: none;' +
-                '}'
-
-            return el
-        }
-
-        const getStyleNodeFrom = (styleFile) => {
-            const el = document.createElement('style');
-
-            if (styleFile) {
-               getRemoteCssFile(styleFile)
-                    .then(css => el.innerText = css);
+        #mapElements = (() => {
+            const getRemoteCssFile = url => {
+                return fetch(url)
+                    .then(response => response.text())
+                    .catch(error => {
+                        const message = `An error has occured: ${response.status}`;
+                        throw new Error(message)
+                    })
             }
 
-            return el;
-        }
-
-        const getWcNode = () => {
-            const el = document.createElement('div');
-            el.id = 'map'
-            el.className = 'leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom'
-            el.tabIndex = '0'
-
-            el.innerHTML =
-                '<div class="leaflet-pane leaflet-map-pane" style="transform: translate3d(0px, 0px, 0px);">' +
-                    '<div class="leaflet-pane leaflet-tile-pane">' +
-                        '<div class="leaflet-layer " style="z-index: 1; opacity: 1;">' +
-                            '<div class="leaflet-tile-container leaflet-zoom-animated"' +
-                                'style="z-index: 19; transform: translate3d(0px, 0px, 0px) scale(1);">' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="leaflet-pane leaflet-overlay-pane"></div>' +
-                    '<div class="leaflet-pane leaflet-shadow-pane"></div>' +
-                    '<div class="leaflet-pane leaflet-marker-pane"></div>' +
-                    '<div class="leaflet-pane leaflet-tooltip-pane"></div>' +
-                    '<div class="leaflet-pane leaflet-popup-pane"></div>' +
-                    '<div class="leaflet-proxy leaflet-zoom-animated"' +
-                        'style="transform: translate3d(1048050px, 697379px, 0px) scale(4096);"></div>' +
-                '</div>' +
-                '<div class="leaflet-control-container">' +
-                    '<div class="leaflet-top leaflet-left">' +
-                        '<div class="leaflet-control-zoom leaflet-bar leaflet-control"><a class="leaflet-control-zoom-in" href="#"' +
-                            'title="Zoom in" role="button" aria-label="Zoom in" aria-disabled="false"><span' +
-                            'aria-hidden="true">+</span></a><a class="leaflet-control-zoom-out" href="#" title="Zoom out"' +
-                            'role="button" aria-label="Zoom out" aria-disabled="false"><span aria-hidden="true">−</span></a>' +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="leaflet-top leaflet-right"></div>' +
-                    '<div class="leaflet-bottom leaflet-left"></div>' +
-                    '<div class="leaflet-bottom leaflet-right">' +
-                        '<div class="leaflet-control-attribution leaflet-control"><a href="https://leafletjs.com"' +
-                            'title="A JavaScript library for interactive maps">Leaflet</a> <span aria-hidden="true">|</span> © <a' +
-                            'href="http://www.openstreetmap.org/copyright">OpenStreetMap</a></div>' +
-                    '</div>' +
-                '</div>'
-
-            return el
-        }
-
-        const getLeafletStyleNode = () => {
-            const leafletCss = {
-                url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-                integrity: 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY='
-            };
-
-            const el = document.createElement('style');
-            getRemoteCssFile(leafletCss.url)
-                .then(css => el.innerText = css)
-
-            return el;
-        }
-
-        const getMarkerClusterStyleNode = () => {
-            const getMarkerClusterStyle = cssFile => {
+            const getWcStyleNode = () => {
                 const el = document.createElement('style');
-                const url = `https://unpkg.com/leaflet.markercluster@1.5.3/dist/${cssFile}`;
-                getRemoteCssFile(url)
-                    .then(css => el.innerText = css);
+                el.innerText =
+                    ':host {' +
+                        'display: block;' +
+                    '}' +
+                    '#map {' +
+                        'position: absolute;' +
+                        'inset: 0px;' +
+                        'top: 0;' +
+                        'right: 0;' +
+                        'bottom: 0;' +
+                        'left: 0;' +
+                    '}' +
+                    '.leaflet-container {' +
+                        'max-width: 100%;' +
+                        'max-height: 100%;' +
+                    '}' +
+                    '.leaflet-control-zoom {' +
+                        'display: none;' +
+                    '}'
+
+                return el
+            }
+
+            const getStyleNodeFrom = (styleFile) => {
+                const el = document.createElement('style');
+
+                if (styleFile) {
+                   getRemoteCssFile(styleFile)
+                        .then(css => el.innerText = css);
+                }
 
                 return el;
-            };
+            }
 
-            return [
-                getMarkerClusterStyle('MarkerCluster.Default.css'),
-                getMarkerClusterStyle('MarkerCluster.css')
-            ];
-        }
+            const getWcNode = () => {
+                const el = document.createElement('div');
+                el.id = 'map'
+                el.className = 'leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom'
+                el.tabIndex = '0'
 
-        return {
-            getWcStyleNode,
-            getStyleNodeFrom,
-            getWcNode,
-            getLeafletStyleNode,
-            getMarkerClusterStyleNode
-        }
-    })();
+                el.innerHTML =
+                    '<div class="leaflet-pane leaflet-map-pane" style="transform: translate3d(0px, 0px, 0px);">' +
+                        '<div class="leaflet-pane leaflet-tile-pane">' +
+                            '<div class="leaflet-layer " style="z-index: 1; opacity: 1;">' +
+                                '<div class="leaflet-tile-container leaflet-zoom-animated"' +
+                                    'style="z-index: 19; transform: translate3d(0px, 0px, 0px) scale(1);">' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="leaflet-pane leaflet-overlay-pane"></div>' +
+                        '<div class="leaflet-pane leaflet-shadow-pane"></div>' +
+                        '<div class="leaflet-pane leaflet-marker-pane"></div>' +
+                        '<div class="leaflet-pane leaflet-tooltip-pane"></div>' +
+                        '<div class="leaflet-pane leaflet-popup-pane"></div>' +
+                        '<div class="leaflet-proxy leaflet-zoom-animated"' +
+                            'style="transform: translate3d(1048050px, 697379px, 0px) scale(4096);"></div>' +
+                    '</div>' +
+                    '<div class="leaflet-control-container">' +
+                        '<div class="leaflet-top leaflet-left">' +
+                            '<div class="leaflet-control-zoom leaflet-bar leaflet-control"><a class="leaflet-control-zoom-in" href="#"' +
+                                'title="Zoom in" role="button" aria-label="Zoom in" aria-disabled="false"><span' +
+                                'aria-hidden="true">+</span></a><a class="leaflet-control-zoom-out" href="#" title="Zoom out"' +
+                                'role="button" aria-label="Zoom out" aria-disabled="false"><span aria-hidden="true">−</span></a>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div class="leaflet-top leaflet-right"></div>' +
+                        '<div class="leaflet-bottom leaflet-left"></div>' +
+                        '<div class="leaflet-bottom leaflet-right">' +
+                            '<div class="leaflet-control-attribution leaflet-control"><a href="https://leafletjs.com"' +
+                                'title="A JavaScript library for interactive maps">Leaflet</a> <span aria-hidden="true">|</span> © <a' +
+                                'href="http://www.openstreetmap.org/copyright">OpenStreetMap</a></div>' +
+                        '</div>' +
+                    '</div>'
+
+                return el
+            }
+
+            const getLeafletStyleNode = () => {
+                const leafletCss = {
+                    url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+                    integrity: 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY='
+                };
+
+                const el = document.createElement('style');
+                getRemoteCssFile(leafletCss.url)
+                    .then(css => el.innerText = css)
+
+                return el;
+            }
+
+            const getMarkerClusterStyleNode = () => {
+                const getMarkerClusterStyle = cssFile => {
+                    const el = document.createElement('style');
+                    const url = `https://unpkg.com/leaflet.markercluster@1.5.3/dist/${cssFile}`;
+                    getRemoteCssFile(url)
+                        .then(css => el.innerText = css);
+
+                    return el;
+                };
+
+                return [
+                    getMarkerClusterStyle('MarkerCluster.Default.css'),
+                    getMarkerClusterStyle('MarkerCluster.css')
+                ];
+            }
+
+            return {
+                getWcStyleNode,
+                getStyleNodeFrom,
+                getWcNode,
+                getLeafletStyleNode,
+                getMarkerClusterStyleNode
+            }
+        })();
+    }
 
     const getLeafletScriptNode = () => {
         const dependency = {
